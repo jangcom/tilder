a6c8eab3eef58b9a492cd0518ed9f46703832387
jang <jangj@korea.ac.kr>
2020-05-03 16:37:26 +0900

    pycodestyle

--- docs/run_pycodestyle.bat
+++ docs/run_pycodestyle.bat
@@ -1,4 +1,2 @@
 @echo off
-rem Do not name this batch file as 'pycodestyle';
-rem otherwise, the pycodestyle executable will be run without arguments.
-pycodestyle --first ./tilder.py
+pycodestyle --first ../tilder.py
